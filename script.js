e0726ad4cf9399439dfa723ddbafce6be1446f11
Namitha S <114816602+Namitha-KS@users.noreply.github.com>
2024-06-23 12:16:41 +0000

    products update

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const wrapper = document.querySelector('.sliderWrapper');
-
 const menuItems = document.querySelectorAll('.menuItem');
 
 const products = [
@@ -8,14 +7,8 @@ const products = [
     title: 'Air Force',
     price: 119,
     colors: [
-      {
-        code: 'black',
-        img: './img/air.png',
-      },
-      {
-        code: 'darkblue',
-        img: './img/air2.png',
-      },
+      { code: 'black', img: './img/air.png' },
+      { code: 'darkblue', img: './img/air2.png' },
     ],
   },
   {
@@ -23,14 +16,8 @@ const products = [
     title: 'Air Jordan',
     price: 149,
     colors: [
-      {
-        code: 'lightgray',
-        img: './img/jordan.png',
-      },
-      {
-        code: 'green',
-        img: './img/jordan2.png',
-      },
+      { code: 'lightgray', img: './img/jordan.png' },
+      { code: 'green', img: './img/jordan2.png' },
     ],
   },
   {
@@ -38,14 +25,8 @@ const products = [
     title: 'Blazer',
     price: 109,
     colors: [
-      {
-        code: 'lightgray',
-        img: './img/blazer.png',
-      },
-      {
-        code: 'green',
-        img: './img/blazer2.png',
-      },
+      { code: 'lightgray', img: './img/blazer.png' },
+      { code: 'green', img: './img/blazer2.png' },
     ],
   },
   {
@@ -53,14 +34,8 @@ const products = [
     title: 'Crater',
     price: 129,
     colors: [
-      {
-        code: 'black',
-        img: './img/crater.png',
-      },
-      {
-        code: 'lightgray',
-        img: './img/crater2.png',
-      },
+      { code: 'black', img: './img/crater.png' },
+      { code: 'lightgray', img: './img/crater2.png' },
     ],
   },
   {
@@ -68,19 +43,15 @@ const products = [
     title: 'Hippie',
     price: 99,
     colors: [
-      {
-        code: 'gray',
-        img: './img/hippie.png',
-      },
-      {
-        code: 'black',
-        img: './img/hippie2.png',
-      },
+      { code: 'gray', img: './img/hippie.png' },
+      { code: 'black', img: './img/hippie2.png' },
     ],
   },
 ];
 
+
 let chosenProduct = products[0];
+
 
 const currentProductImg = document.querySelector('.productImage');
 const currentProductTitle = document.querySelector('.productTitle');
@@ -88,42 +59,53 @@ const currentProductPrice = document.querySelector('.productPrice');
 const currentProductColors = document.querySelectorAll('.color');
 const currentProductSizes = document.querySelectorAll('.size');
 
+const updateProductDisplay = () => {
+  currentProductTitle.textContent = chosenProduct.title.toUpperCase();
+  currentProductPrice.textContent = '$' + chosenProduct.price;
+  currentProductImg.src = chosenProduct.colors[0].img;
+  currentProductColors.forEach((color, index) => {
+    if (chosenProduct.colors[index]) {
+      color.style.backgroundColor = chosenProduct.colors[index].code;
+      color.style.display = 'block';
+    } else {
+      color.style.display = 'none';
+    }
+  });
+};
+
+
 menuItems.forEach((item, index) => {
   item.addEventListener('click', () => {
-    //changes the current slide
+ 
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
 
-    //changes the chosen product
+  
     chosenProduct = products[index];
 
-    //changes the texts of current product
-    currentProductTitle.textContent = chosenProduct.title.toUpperCase();
-    currentProductPrice.textContent = '$' + chosenProduct.price;
-    currentProductImg.src = chosenProduct.colors[0].img;
-
-    //changes the style of product by selected color
-    currentProductColors.forEach((color, index) => {
-      color.style.backgroundColor = chosenProduct.colors[index].code;
-    });
+    
+    updateProductDisplay();
   });
 });
 
+// Handle color selection
 currentProductColors.forEach((color, index) => {
   color.addEventListener('click', () => {
     currentProductImg.src = chosenProduct.colors[index].img;
   });
 });
 
-currentProductSizes.forEach((size, index) => {
+
+currentProductSizes.forEach((size) => {
   size.addEventListener('click', () => {
-    currentProductSizes.forEach((size) => {
-      size.style.backgroundColor = 'white';
-      size.style.color = 'black';
+    currentProductSizes.forEach((s) => {
+      s.style.backgroundColor = 'white';
+      s.style.color = 'black';
     });
     size.style.backgroundColor = 'black';
     size.style.color = 'white';
   });
 });
+
 
 const productButton = document.querySelector('.productButton');
 const payment = document.querySelector('.payment');
@@ -132,6 +114,10 @@ const close = document.querySelector('.close');
 productButton.addEventListener('click', () => {
   payment.style.display = 'flex';
 });
+
 close.addEventListener('click', () => {
   payment.style.display = 'none';
 });
+
+// Initial product display update
+updateProductDisplay();
