3cfaf23f317531e1f46b948df22706d39b993a89
Namitha S <114816602+Namitha-KS@users.noreply.github.com>
2024-06-23 01:12:24 +0000

    shoe-store

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// js/main.js
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('includes/navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar").innerHTML = data;
-        });
-});
